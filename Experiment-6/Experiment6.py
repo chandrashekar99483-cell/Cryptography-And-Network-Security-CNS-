8b721c3ab69f1af21a2e5f3861b3e1f7807df57c
CHANDRA SHEKAR RAO P <chandrashekar99483@gmail.com>
2026-09-08 11:32:10 +0530

    Update Experiment6.py

--- Experiment-6/Experiment6.py
+++ Experiment-6/Experiment6.py
@@ -40,47 +40,6 @@ def fermat_test(n, rounds=5):
     return True
 
 
-# ---------------- Miller-Rabin Test ---------------- #
-
-def miller_rabin(n, rounds=5):
-    if n <= 1:
-        return False
-
-    if n == 2 or n == 3:
-        return True
-
-    if n % 2 == 0:
-        return False
-
-    # Write n - 1 as 2^r * d
-    d = n - 1
-    r = 0
-
-    while d % 2 == 0:
-        d //= 2
-        r += 1
-
-    # Perform multiple rounds
-    for _ in range(rounds):
-
-        a = random.randint(2, n - 2)
-
-        x = pow(a, d, n)
-
-        if x == 1 or x == n - 1:
-            continue
-
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-
-            if x == n - 1:
-                break
-
-        else:
-            return False
-
-    return True
-
 
 # ---------------- Main Program ---------------- #
 
